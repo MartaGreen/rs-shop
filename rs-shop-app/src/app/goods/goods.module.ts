35bef7ede1a82f0796ae4aa56d6cadee33aa3357
martagreen <portnykhmaria@gmail.com>
2021-09-18 13:26:52 +0400

    fix: project problems

--- rs-shop-app/src/app/goods/goods.module.ts
+++ rs-shop-app/src/app/goods/goods.module.ts
@@ -1,14 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { GoodsCatalogComponent } from './components/goods-catalog/goods-catalog.component';
+import { GoodsDetailsComponent } from './components/goods-details/goods-details.component';
 import { GoodsPageComponent } from './components/goods-page/goods-page.component';
+import { SortComponent } from './components/goods-page/sort/sort.component';
 import { GoodsRoutingModule } from './goods-routhing.module';
 import { GoodsService } from './services/goods.service';
-import { GoodsDetailsComponent } from './components/goods-details/goods-details.component';
-import { SortComponent } from './components/goods-page/sort/sort.component';
 
 @NgModule({
-  declarations: [GoodsCatalogComponent, GoodsPageComponent, GoodsDetailsComponent, SortComponent],
+  declarations: [
+    GoodsCatalogComponent,
+    GoodsPageComponent,
+    GoodsDetailsComponent,
+    SortComponent,
+  ],
   imports: [CommonModule, GoodsRoutingModule],
   providers: [GoodsService],
 })
